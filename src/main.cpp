704bb78d089c6f170078804a52a06d80f9ac1b0a
quocan0303 <quocan3399@gmail.com>
2023-10-09 12:44:13 +0700

    Remove PS4 Protocal

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
 // #include <Adafruit_MPU6050.h>
 #include <Arduino.h>
-#include "../lib/ps4_control/ps4_control.h"
-#include <PS4Controller.h>
-// #include "../lib/esp_wifi/init_esp_wifi.h"
+#include "../lib/esp_wifi/init_esp_wifi.h"
 
 void setup(){
   // Start serial with platform baudrate/monitor speed
   Serial.begin(115200);
-  // pinMode(13, OUTPUT);
-  // digitalWrite(13, HIGH);
-  init_ps4();
-  // init_esp();
-  PS4.begin();
+  init_esp(); // Initialize ESP NOW protocol
   Serial.println("Ready.");
 }
 
 void loop() {
-  checkInput();
+  
 }
 
